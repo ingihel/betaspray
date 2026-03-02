c1c27f921c368208a3d34fb1744afbe7350e596f
Prakhar <visitprakhar@gmail.com>
2026-03-01 18:20:48 -0600

    Fuck my stupid chud life

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if docker image inspect esp32 &>/dev/null; then
-    docker run --rm -it --name esp32_dev -v "$(pwd)":/project esp32
-else
-    docker build -t esp32 .
-fi
