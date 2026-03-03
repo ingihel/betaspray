aee90700c2cab326273f6e12040bdd5a1f948c58
Ingi Helgason <ingih2@illinois.edu>
2026-03-03 14:50:33 -0600

    Working flash config

--- src/main.c
+++ src/main.c
@@ -1,1 +1,21 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN 2
+
+void app_main(void) {
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        printf("LED ON\n");
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Wait 1 second
+        
+        printf("LED OFF\n");
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Wait 1 second
+    }
+}
